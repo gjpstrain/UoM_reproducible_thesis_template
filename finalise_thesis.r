5192d61dafd4bfa177b91e6514d0c0c55c8a81ac
gjpstrain <gabrielstrain@gmail.com>
2026-03-19 10:55:01 +0000

    restarted thesis template work

--- finalise_thesis.r
+++ finalise_thesis.r
@@ -1,51 +1,56 @@
-# First, run all .qmd chapters
+# If you only want to compile the final thesis, go straight to line 53
 
-#install.packages("quarto")
-#install.packages("tinytex")
+# If you want to compile each chapter from scratch, start from line 10
+
+# NB if you are running the Docker container, you don't need to install 
+# additional tex packages; this is handled by the Dockerfile
+
+################################################################################
 
 library(quarto)
 library(tinytex)
 
+# Following command renders all quarto chapters:
+
 quarto_render("chapters_quarto/", output_format = "latex")
 
-################################################################################
+# Following command renders only a single chapter:
+
+quarto_render("chapters_quarto/7_belief_change.qmd", output_format = "latex")
 
 # functions to reformat tex are stored in the local directory
+# these must be run before rendering the final pdf
 
-source("reformat_tex.r")
+source("reformat_tex.R")
 
 folder_path <- "chapters_tex/chapters_quarto/"
 replace_before_document_in_folder(folder_path)
 
 ################################################################################
 
-# now we can use tinytex to compile the thesis
-# if this is your first time using the template,
-# you will need to install tinytex (the package), and
-# then TinyTex (the engine)
+# Skip straight to line 53 if you are using the Docker container.
 
-#install.packages("tinytex")
-#tinytex::install_tinytex()
+## now we can use tinytex to compile the thesis
+## if this is your first time using the template without using the Docker Container,
+## you will need to install tinytex (the package), and
+## then TinyTex (the engine)
 
-# because the UoM template uses lualatex, we may have to manually install
-# an additional package (just uncomment line 30)
+# install.packages("tinytex")
+# tinytex::reinstall_tinytex()
 
-#tinytex::tlmgr_install("luatex85")
+## because the UoM template uses lualatex, we have to manually install
+## an additional package (just uncomment and run)
 
-#sometimes the Tex Gyres Termes font is not found, so manually install it via
-#tinytex
+# tinytex::tlmgr_install("luatex85")
 
-#tinytex::tlmgr_install("tex-gyre")
+## sometimes the Tex Gyres Termes font is not found, so manually install it via
+## tinytex
 
-tinytex::lualatex("main.tex", bib_engine = "biber") 
+# tinytex::tlmgr_install("tex-gyre")
 
+# Use the below command to render the final pdf
 
-
-
-
-
-
-
+tinytex::lualatex("main.tex", bib_engine = "biber")
 
 
 
